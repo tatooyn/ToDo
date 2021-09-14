8fc944dd3b2856db883d79f851a3af38c8d836cf
Andres <tatooyn@gmail.com>
2021-09-14 15:31:03 -0500

    gitHub

--- src/Components/MyAppDesc.js
+++ src/Components/MyAppDesc.js
@@ -4,7 +4,7 @@ const MyAppDesc= ()=>{
     return <div className='MyAppDesc'>
         <p>Aplicación desarrollada con React, por Andrés Cardona Alvarez</p>
         <p>Del curso "React: De cero a experto ( Hooks y MERN ); de Udemy"</p>
-        <p>GitHub: <a href='https://github.com/tatooyn/react-gifts.git'>https://github.com/tatooyn/react-gifts.git</a> </p>
+        <p>GitHub: <a href='https://github.com/tatooyn/ToDo.git'>https://github.com/tatooyn/react-gifts.git</a> </p>
         <h4>Temas:</h4>
         <ul>
             <li>useReducer</li>
